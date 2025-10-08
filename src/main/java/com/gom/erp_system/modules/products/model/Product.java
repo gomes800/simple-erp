a637cc30ab8cb87e7998072c64d3d859b23bb18d
gom-trab <jgomesleite8@gmail.com>
2025-10-08 13:04:23 -0300

    feat: create product model

---
+++ src/main/java/com/gom/erp_system/modules/products/model/Product.java
@@ -0,0 +1,49 @@
+package com.gom.erp_system.modules.products.model;
+
+import com.gom.erp_system.modules.products.model.enums.Categories;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String code;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String description;
+
+    private BigDecimal costPrice;
+    private BigDecimal salePrice;
+    private Integer stock;
+    private Integer minimumStock;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Categories category;
+
+    private LocalDateTime registerDate;
+    private String supplier;
+}
---
+++ src/main/java/com/gom/erp_system/modules/products/model/enums/Categories.java
@@ -0,0 +1,16 @@
+package com.gom.erp_system.modules.products.model.enums;
+
+public enum Categories {
+    CLOTHING,
+    ELECTRONICS,
+    FOOD,
+    BEVERAGES,
+    BOOKS,
+    BEAUTY,
+    SPORTS,
+    TOYS,
+    HOME,
+    AUTOMOTIVE,
+    OFFICE,
+    OTHER
+}
